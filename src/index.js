f32a76059259a6c84fbf452bc4e51fd914f7be29
ROHAN4535 <grohan468@gmail.com>
2023-12-11 23:25:44 +0530

    Compose and send Mail

--- src/index.js
+++ src/index.js
@@ -5,12 +5,16 @@ import "bootstrap/dist/js/bootstrap.min.js";
 import "./index.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import store from "./Store";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+    <Provider store={store}>
+        <App />
+      </Provider>
     </BrowserRouter>
   </React.StrictMode>
 );
